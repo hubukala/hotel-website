9ad08222db72b21b6f9b99437484ddfa8ea2b7ad
Hubert Bukala <hubertbukala@icloud.com>
2022-10-09 12:58:13 +0200

    added scrolling to section function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -8,6 +8,29 @@ const guestNotValid = document.getElementById("guest-not-valid");
 const locationNotValid = document.getElementById("location-not-valid");
 const dateNotValid = document.getElementById("date-not-valid");
 
+const recommendedButton = document.getElementById("recommended-btn");
+const benefitsButton = document.getElementById("benefits-btn");
+const feedbackButton = document.getElementById("feedback-btn");
+
+const sectionRecommended = document.getElementById("section-recommended");
+const sectionBenefits = document.getElementById("section-benefits");
+const sectionFeedback = document.getElementById("section-feedback");
+
+recommendedButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  sectionRecommended.scrollIntoView(true);
+});
+
+benefitsButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  sectionBenefits.scrollIntoView(true);
+});
+
+feedbackButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  sectionFeedback.scrollIntoView(true);
+});
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   checkInputs();
